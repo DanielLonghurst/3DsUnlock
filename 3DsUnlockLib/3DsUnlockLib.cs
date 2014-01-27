e5ae20774ca9cbea292d832bf578788701633230
DanielLonghust <d_longhurst@hotmail.com>
2014-01-26 22:38:54 -0500

    Added license headers to code

--- 3DsUnlockLib/3DsUnlockLib.cs
+++ 3DsUnlockLib/3DsUnlockLib.cs
@@ -1,5 +1,24 @@
-using System;
+// Copyright (c) 2014 InVision Automation, Inc., Dallas, Georgia
+//
+// 3DSUnlock(tm) is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// 3DSUnlock(tm) is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the source code.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Daniel V. Longhurst
 
+/**************************************************
+20140126	Initial version of the library in c# code
+
+***************************************************/
 
 
 namespace ThreeDSUnlockLib
